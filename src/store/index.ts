1a410ba46c6d11470a207d4a53e46502742ec936
Egor O. Chindyasov <egor@chindyasov.ru>
2022-09-07 16:27:28 +0300

    added new statets, getters and mutations

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,13 +3,27 @@ import {createStore} from 'vuex'
 export default createStore({
     state: {
         floorsNumber: 5,
+        callFloor: 1,
+        elevator: {
+            position: 1,
+            isMoved: false
+        }
     },
     getters: {
         getFloorsNumber: (state) => {
             return state.floorsNumber
+        },
+        getCallFloor: (state) => {
+            return state.callFloor
+        },
+        getElevator: (state) => {
+            return state.elevator
         }
     },
     mutations: {
+        changeCallFloor: (state, newCallFloor: number) => {
+            state.callFloor = newCallFloor
+        }
     },
     actions: {
     },
