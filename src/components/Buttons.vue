f4f34e2203b94d8a8bc7654fbe065487df3e8e99
Egor O. Chindyasov <egor@chindyasov.ru>
2022-09-07 22:49:24 +0300

    added flashing effect for floor button

--- src/components/Buttons.vue
+++ src/components/Buttons.vue
@@ -6,29 +6,89 @@
             :key="count"
             :style="{width: '100%', height: `${500 / floorsNumber}px`}"
         >
-            <button 
+            <div 
+                class="button" 
                 @click="callElevator(floorsNumber - count + 1)"
             >
-                Вызвать
-            </button>
+                <div 
+                    class="buttonInner"
+                    ref="buttonRef"
+                    :style="{borderColor: setButtonColor(floorsNumber - count + 1)}"
+                ></div>
+            </div>
         </div>
     </div>
 </template>
-  
+
 <script lang="ts">
-import {defineComponent} from 'vue'
+import {
+    ref, 
+    computed, 
+    watch, 
+    defineComponent
+} from 'vue'
 import {useStore} from 'vuex'
   
 export default defineComponent({
     setup() {
         const store = useStore()
+
+        const elevatorPosition = ref(store.state.elevatorPosition)
+        const nextElevatorPosition = ref(store.getters.getNextCallFloor)
+        const isMoved = ref(store.state.isMoved)
+        const buttonRef = ref<HTMLDivElement | HTMLDivElement[] | null>(null)
+
+        const elevatorPositionComputed = computed(() => store.getters.getElevatorPositon)
+        const nextElevatorPositionComputed = computed(() => store.getters.getNextCallFloor)
+        const isMovedComputed = computed(() => store.getters.getIsMoved)
+        const queueCallsComputed = computed(() => store.getters.getQueueCalls)
+
+        watch(elevatorPositionComputed, (newValue, oldValue) => {
+            elevatorPosition.value = newValue
+        })
+
+        watch(isMovedComputed, (newValue, oldValue) => {
+            isMoved.value = newValue
+
+            if (!newValue) {
+                // @ts-ignore
+                if (buttonRef._rawValue) {
+                    // @ts-ignore
+                    buttonRef._rawValue[store.getters.getFloorsNumber - nextElevatorPosition.value].animate(
+                        [
+                            {opacity: 1, borderColor: 'red'},
+                            {opacity: 0, borderColor: 'red'},
+                            {opacity: 1, borderColor: 'red'}
+                        ], 
+                        {
+                            duration: 1000,
+                            iterations: 3
+                        }
+                    )
+                }
+            }
+        })
+
+        watch(nextElevatorPositionComputed, (newValue, oldValue) => {
+            nextElevatorPosition.value = newValue
+        })
+
         const callElevator = (count: number) => {
-            store.commit('changeCallFloor', count)
+            store.commit('addToQueueCalls', count)
+        }
+
+        const setButtonColor = (count: number) => {
+            return queueCallsComputed.value.includes(count) ? 'red' : 'black'
         }
 
         return {
             floorsNumber: store.getters.getFloorsNumber,
-            callElevator
+            elevatorPosition,
+            nextElevatorPosition,
+            isMoved,
+            callElevator,
+            setButtonColor,
+            buttonRef
         }
     }
 })
@@ -44,6 +104,26 @@ export default defineComponent({
             display: flex;
             justify-content: center;
             align-items: center;
+
+            .button {
+                width: 30px;
+                height: 30px;
+                border: 3px solid black;
+                box-sizing: border-box;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                color: black;
+                cursor: pointer;
+
+                .buttonInner {
+                    width: 10px;
+                    height: 10px;
+                    border: 3px solid black;
+                    border-radius: 50%;
+                    opacity: 1;
+                }
+            }
         }
     }
 </style>
