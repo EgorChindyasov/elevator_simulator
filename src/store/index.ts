eea15a01ea0fc5b23a2553b1c643e64da7013897
Egor O. Chindyasov <egor@chindyasov.ru>
2022-09-07 22:46:58 +0300

    added new states and methods

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,26 +3,44 @@ import {createStore} from 'vuex'
 export default createStore({
     state: {
         floorsNumber: 5,
-        callFloor: 1,
-        elevator: {
-            position: 1,
-            isMoved: false
-        }
+        elevatorPosition: 1,
+        isMoved: false,
+        queueCalls: []
     },
     getters: {
         getFloorsNumber: (state) => {
             return state.floorsNumber
         },
-        getCallFloor: (state) => {
-            return state.callFloor
+        getElevatorPositon: (state) => {
+            return state.elevatorPosition
         },
-        getElevator: (state) => {
-            return state.elevator
+        getIsMoved: (state) => {
+            return state.isMoved
+        },
+        getNextCallFloor: (state) => {
+            return state.queueCalls[0] ?? state.elevatorPosition
+        },
+        getQueueCalls: (state) => {
+            return state.queueCalls
         }
     },
     mutations: {
-        changeCallFloor: (state, newCallFloor: number) => {
-            state.callFloor = newCallFloor
+        changeElevatorPosition: (state, newElevatorPosition: number) => {
+            state.elevatorPosition = newElevatorPosition
+        },
+        changeIsMoved: (state, payload: boolean) => {
+            state.isMoved = payload
+        },
+        addToQueueCalls: (state, payload: number) => {
+            const {queueCalls, elevatorPosition} = state
+
+            if (queueCalls[queueCalls.length - 1] !== payload && elevatorPosition !== payload) {
+                // @ts-ignore
+                queueCalls.push(payload)
+            }
+        },
+        removeFromQueueCalls: (state) => {
+            state.queueCalls.shift()
         }
     },
     actions: {
